4731fa662a325f1a4170512b2d8994e21f96d19f
Archong Liu <s0906170155@gmail.com>
2025-11-27 09:01:57 +0000

    feat:Add test program

--- code/test/test1.c
+++ code/test/test1.c
@@ -1,7 +1,18 @@
 #include "syscall.h"
-main()
-	{
-		int	n;
-		for (n=9;n>0;n--);
-			//PrintInt(n);
-	}
+int main() {
+    // 故意宣告一個大陣列，超過 4KB（NachOS 預設主記憶體）
+    // int 大小 4 bytes -> 2000 個 int 約 8000 bytes
+    const int N = 2000;
+    volatile int a[N];
+    int i;
+    int sum = 0;
+    for (i = 0; i < N; i++) {
+        a[i] = i;
+    }
+    for (i = 0; i < N; i++) {
+        sum += a[i];
+    }
+    PrintInt(sum);    // 或使用 printf/Write depending on your NachOS test harness
+    Halt();
+    return 0;
+}
