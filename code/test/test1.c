eb1b160b58cf403d310fd24a3dda762f563b2396
Archong Liu <s0906170155@gmail.com>
2025-11-28 17:00:39 +0000

    fix:modify test programs

--- code/test/test1.c
+++ code/test/test1.c
@@ -1,6 +1,6 @@
 #include "syscall.h"
 
-#define N 20000     // 2k integers = 8KB，保證多頁
+#define N 1000   // 2k integers = 8KB，保證多頁
 
 int bigArray[N];    // 放 BSS，不佔 stack，不會 crash
 
--- code/test/test2.c
+++ code/test/test2.c
@@ -1,8 +1,25 @@
 #include "syscall.h"
 
-main()
-        {
-                int     n;
-                for (n=2020;n<=2030;n++)
-                        PrintInt(n);
-        }
+#define N 1000
+volatile int a[N];
+
+int main() {
+    int i;
+    int sum;
+
+    sum = 0;
+    i=0;
+    // 調 3 個熱區，不連續
+    for (i = 0; i < N; i += 3) a[i] = i;
+    for (i = 1; i < N; i += 3) a[i] = i;
+    for (i = 2; i < N; i += 3) a[i] = i;
+
+    // 交錯讀取 3 區域
+    for (i = 0; i < N; i++) {
+        sum += a[(i*7) % N]; // 用跳躍式 pattern
+    }
+
+    PrintInt(sum);
+    Halt();
+    return 0;
+}
